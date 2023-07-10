b4050c96dfc33448a918c4745858f03f7d1b9f1c
Min-jae Kim <mink4661@snu.ac.kr>
2023-07-11 01:22:50 +0900

    Feat: add tabs into app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { AuthStack, CommentaryTab, SigonganTab } from './navigations';
+
 import { UserStateProvider, useUserState } from './providers';
-import { AuthStack } from './navigations';
 
 const Main = () => {
   const { userState } = useUserState();
 
   return (
     <NavigationContainer>
-      <SafeAreaView>{userState === 'unLogin' && <AuthStack />}</SafeAreaView>
+      <SafeAreaView>
+        {userState === 'unLogin' && <AuthStack />}
+        {userState === 'Sigongan' && <SigonganTab />}
+        {userState === 'Commentary' && <CommentaryTab />}
+      </SafeAreaView>
     </NavigationContainer>
   );
 };
