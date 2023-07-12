ce5288fa949be1b81188e44c78694b3037dd69c3
rbgksqkr <rbgks1937@naver.com>
2023-07-13 00:39:32 +0900

    Feat: #3 - RequestList 홀수일 때 왼쪽 정렬

--- src/components/Comment/Home/RequestList.tsx
+++ src/components/Comment/Home/RequestList.tsx
@@ -1,21 +1,33 @@
-import { View, StyleSheet, Text } from 'react-native';
+import { StyleSheet, FlatList, TouchableOpacity, ScrollView } from 'react-native';
 import { IRequest } from '../../../types/request';
 import RequestItem from './RequestItem';
 
 const RequestList = ({ requestList }: { requestList: IRequest[] }) => {
   return (
-    <View style={styles.cardContainer}>
+    <ScrollView contentContainerStyle={styles.cardContainer}>
       {requestList.map((request: IRequest) => (
         <RequestItem key={request.id} request={request} />
       ))}
-    </View>
+    </ScrollView>
+    // <FlatList
+    //   keyExtractor={(item, index) => index.toString()}
+    //   data={requestList}
+    //   contentContainerStyle={{ width: '90%', marginLeft: 35 }}
+    //   renderItem={({ item }) => (
+    //     <TouchableOpacity>
+    //       <RequestItem key={item.id} request={item} />
+    //     </TouchableOpacity>
+    //   )}
+    //   columnWrapperStyle={{ gap: 20 }}
+    //   ItemSeparatorComponent={() => <View style={{ height: 20 }} />}
+    //   numColumns={2}
+    // ></FlatList>
   );
 };
 
 const styles = StyleSheet.create({
   cardContainer: {
-    display: 'flex',
-    justifyContent: 'center',
+    marginLeft: 35,
     flexDirection: 'row',
     flexWrap: 'wrap',
     gap: 30,
