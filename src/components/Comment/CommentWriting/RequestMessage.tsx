323b163b0848fef55dbfcdd06f1d33767d891454
rbgksqkr <rbgks1937@naver.com>
2023-07-16 00:11:13 +0900

    Feat: #7 - 해설 작성하기 전 페이지 대화부분 구현

---
+++ src/components/Comment/CommentWriting/RequestMessage.tsx
@@ -0,0 +1,43 @@
+import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView } from 'react-native';
+
+const RequestMessage = () => {
+  return (
+    <View style={styles.chatContainer}>
+      <View>
+        <Image source={require('../../../../assets/sample_request.png')} alt="" style={styles.chatImage} />
+      </View>
+      <View style={styles.chatText}>
+        <Text>고양이가 어떤 모습인지 자세히 설명해주세요.</Text>
+      </View>
+      <View>
+        <Text style={{ color: '#777' }}>오후 2:05</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  chatContainer: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    width: '90%',
+    gap: 10,
+    height: 70,
+  },
+  chatImage: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+  },
+  chatText: {
+    width: '60%',
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 15,
+    borderBottomLeftRadius: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default RequestMessage;
--- src/screens/Comment/CommentWritingScreen.tsx
+++ src/screens/Comment/CommentWritingScreen.tsx
@@ -1,7 +1,7 @@
 import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView } from 'react-native';
-import { Shadow } from 'react-native-shadow-2';
 import Footer from '../../components/Comment/CommentWriting/Footer';
 import Header from '../../components/Comment/CommentWriting/Header';
+import RequestMessage from '../../components/Comment/CommentWriting/RequestMessage';
 
 const CommentWritingScreen = ({ navigation }: { navigation: any }) => {
   return (
@@ -10,18 +10,8 @@ const CommentWritingScreen = ({ navigation }: { navigation: any }) => {
       <TouchableOpacity style={styles.imageContainer}>
         <Image source={require('../../../assets/sample_comment.png')} alt="" style={styles.requestImage} />
       </TouchableOpacity>
-      <ScrollView style={styles.bodyContainer}>
-        <View>
-          <View>
-            <Text>이미지</Text>
-          </View>
-          <View>
-            <Text>고양이가 어떤 모습인지 자세히 설명해주세요.</Text>
-          </View>
-          <View>
-            <Text>오후 2:05</Text>
-          </View>
-        </View>
+      <ScrollView contentContainerStyle={{ alignItems: 'center' }} style={styles.bodyContainer}>
+        <RequestMessage />
       </ScrollView>
       <Footer />
     </View>
@@ -41,22 +31,6 @@ const styles = StyleSheet.create({
   },
   bodyContainer: {
     flex: 0.7,
-  },
-  footerContainer: {
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-  },
-  commentBtn: {
-    backgroundColor: '#2C2C2C',
-    width: '90%',
-    height: '80%',
-    borderRadius: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  commentText: {
-    color: 'white',
-    fontSize: 22,
   },
 });
 
