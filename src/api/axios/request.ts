e9bb34da0aafa5b2d1b68f8b722592cc39d5fc8a
rbgksqkr <rbgks1937@naver.com>
2023-07-25 02:41:47 +0900

    Feat: #27 - 해설답변하기 API 구현

--- src/api/axios/request.ts
+++ src/api/axios/request.ts
@@ -5,7 +5,7 @@ export const getRequestAll = async (fcmToken: string, token: string) => {
 };
 
 export const getRequest = async (id: string, fcmToken: string, token: string) => {
-  return await Server.get(`/post/${id}`, { headers: { fcmToken, Authorization: token } });
+  return await Server.get(`/post/${id}`, { params: { id }, headers: { fcmToken, Authorization: token } });
 };
 
 export const getProceedRequest = async (fcmToken: string, token: string) => {
@@ -14,4 +14,8 @@ export const getProceedRequest = async (fcmToken: string, token: string) => {
 
 export const getCompletedRequest = async (fcmToken: string, token: string) => {
   return await Server.get(`/post/completed`, { headers: { fcmToken, Authorization: token } });
+};
+
+export const startComment = async (postId: string, text: string, fcmToken: string, token: string) => {
+  return await Server.put(`/post/start`, { postId, text }, { headers: { fcmToken, Authorization: token } });
 };
