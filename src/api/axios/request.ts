d13c1b36a8c75d2f1ad7f295c523585e82bd5d67
rbgksqkr <rbgks1937@naver.com>
2023-07-25 02:51:14 +0900

    Feat: #27 - 해설자 request API 리턴 타입 지정

--- src/api/axios/request.ts
+++ src/api/axios/request.ts
@@ -1,25 +1,46 @@
+import {
+  RequestAllReturnType,
+  RequestReturnType,
+  ProceedRequestReturnType,
+  CompletedRequestReturnType,
+  startCommentReturnType,
+  endCommentReturnType,
+} from '.';
 import { Server } from './setting';
 
 export const getRequestAll = async (fcmToken: string, token: string) => {
-  return await Server.get('/post', { headers: { fcmToken, Authorization: token } });
+  return await Server.get<RequestAllReturnType>('/post', { headers: { fcmToken, Authorization: token } });
 };
 
 export const getRequest = async (id: string, fcmToken: string, token: string) => {
-  return await Server.get(`/post/${id}`, { params: { id }, headers: { fcmToken, Authorization: token } });
+  return await Server.get<RequestReturnType>(`/post/${id}`, {
+    params: { id },
+    headers: { fcmToken, Authorization: token },
+  });
 };
 
 export const getProceedRequest = async (fcmToken: string, token: string) => {
-  return await Server.get(`/post/proceed`, { headers: { fcmToken, Authorization: token } });
+  return await Server.get<ProceedRequestReturnType>(`/post/proceed`, { headers: { fcmToken, Authorization: token } });
 };
 
 export const getCompletedRequest = async (fcmToken: string, token: string) => {
-  return await Server.get(`/post/completed`, { headers: { fcmToken, Authorization: token } });
+  return await Server.get<CompletedRequestReturnType>(`/post/completed`, {
+    headers: { fcmToken, Authorization: token },
+  });
 };
 
 export const startComment = async (postId: string, fcmToken: string, token: string) => {
-  return await Server.put(`/post/start`, { postId }, { headers: { fcmToken, Authorization: token } });
+  return await Server.put<startCommentReturnType>(
+    `/post/start`,
+    { postId },
+    { headers: { fcmToken, Authorization: token } }
+  );
 };
 
 export const endComment = async (postId: string, text: string, fcmToken: string, token: string) => {
-  return await Server.post(`/post/start`, { postId, text }, { headers: { fcmToken, Authorization: token } });
+  return await Server.post<endCommentReturnType>(
+    `/post/start`,
+    { postId, text },
+    { headers: { fcmToken, Authorization: token } }
+  );
 };
