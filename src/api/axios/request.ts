6f4303c0f98742597638028b8ee881c502937169
rbgksqkr <rbgks1937@naver.com>
2023-07-25 02:29:07 +0900

    Feat: #27 - 의뢰 상세보기 API 구현

--- src/api/axios/request.ts
+++ src/api/axios/request.ts
@@ -1,5 +1,9 @@
 import { Server } from './setting';
 
-export const getRequestList = async (fcmToken: string, token: string) => {
+export const getRequestAll = async (fcmToken: string, token: string) => {
   return await Server.get('/post', { headers: { fcmToken, Authorization: token } });
+};
+
+export const getRequest = async (id: string, fcmToken: string, token: string) => {
+  return await Server.get(`/post/${id}`, { headers: { fcmToken, Authorization: token } });
 };
