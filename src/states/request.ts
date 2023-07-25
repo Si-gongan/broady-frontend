42090bf840ee0f8d7d5cf46f7e3a92c2fba4f733
rbgksqkr <rbgks1937@naver.com>
2023-07-25 18:28:50 +0900

    Feat: #27 - id 타입 string으로 수정

--- src/states/request.ts
+++ src/states/request.ts
@@ -4,49 +4,49 @@ import { IRequest } from '../types/request';
 // TODO: 더미 데이터. 데이터 생기면 빈 배열로 두기.
 const initialState: IRequest[] = [
   {
-    id: 0,
+    id: '0',
     createdAt: new Date('2023-07-22T13:40:13Z'),
     text: '고양이가 어떤 모습인지 자세히 설명해주세요.',
     photo: '../../assets/sample_request.png',
   },
   {
-    id: 1,
+    id: '1',
     createdAt: new Date('2023-07-22T13:22:13Z'),
     text: '질문 내용1',
     photo: '../../assets/sample_request.png',
   },
   {
-    id: 2,
+    id: '2',
     createdAt: new Date('2023-07-22T12:12:13Z'),
     text: '질문 내용2',
     photo: '../../assets/sample_request.png',
   },
   {
-    id: 3,
+    id: '3',
     createdAt: new Date('2023-07-21T03:12:13Z'),
     text: '질문 내용3',
     photo: '../../assets/sample_request.png',
   },
   {
-    id: 4,
+    id: '4',
     createdAt: new Date('2023-07-20T03:12:13Z'),
     text: '질문 내용4',
     photo: '../../assets/sample_request.png',
   },
   {
-    id: 5,
+    id: '5',
     createdAt: new Date('2023-07-19T03:12:13Z'),
     text: '질문 내용5',
     photo: '../../assets/sample_request.png',
   },
   {
-    id: 6,
+    id: '6',
     createdAt: new Date('2023-07-18T03:12:13Z'),
     text: '질문 내용6',
     photo: '../../assets/sample_request.png',
   },
   {
-    id: 7,
+    id: '7',
     createdAt: new Date('2023-07-18T03:12:13Z'),
     text: '질문 내용7',
     photo: '../../assets/sample_request.png',
