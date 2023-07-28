15ca9bc2079f36ecd2c1c26c40cdd7b58363d30e
rbgksqkr <rbgks1937@naver.com>
2023-07-28 18:43:24 +0900

    Feat: #36 - 감사인사 화면에 렌더링

--- src/components/Comment/CommentWriting/MessageList.tsx
+++ src/components/Comment/CommentWriting/MessageList.tsx
@@ -16,6 +16,9 @@ interface IChat {
   appreciatedText?: string;
 }
 
+// responseUser : 해설자 측
+// requestedUser : 시각장애인 측
+
 const MessageList = ({ request }: { request: ICurrentRequest }) => {
   const allMessageList = [...request.requestedUser, ...request.responseUser];
   const sortedMessageList = [...allMessageList].sort((a, b) =>
@@ -34,7 +37,18 @@ const MessageList = ({ request }: { request: ICurrentRequest }) => {
         />
       </View>
       {sortedMessageList.map((message: IChat, idx: number) => {
+        // 감사 인사한 경우
+        if (message.userId && message.appreciated && message.appreciatedText) {
+          return (
+            <>
+              <ResponseMessage key={idx} comment={message} />
+              <RequestMessage key={idx} content={{ ...message, text: message.appreciatedText }} />
+            </>
+          );
+        }
+        // 해설자 측 답변
         if (message.userId) return <ResponseMessage key={idx} comment={message} />;
+        // 시각장애인 측 의뢰
         else return <RequestMessage key={idx} content={message} />;
       })}
     </>
