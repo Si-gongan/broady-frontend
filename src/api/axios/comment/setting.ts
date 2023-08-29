14199480b514737730b40a1ee1c39665f55ad28f
Min-jae Kim <mink4661@snu.ac.kr>
2023-08-29 15:56:22 +0900

    feat: switch server to comment-server

---
+++ src/api/axios/comment/setting.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+export const CommentServer = axios.create({
+  baseURL: process.env.EXPO_PUBLIC_API_SERVER_URL,
+  timeout: 25000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// CommentServer Setting
+CommentServer.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+CommentServer.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
