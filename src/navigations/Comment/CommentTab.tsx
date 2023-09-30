64433007c41902e648b6a82ed921fa346c63e68e
rbgksqkr <rbgks1937@naver.com>
2023-10-01 00:52:12 +0900

    Design: #78 - 하단탭 디자인 리뉴얼

--- src/navigations/Comment/CommentTab.tsx
+++ src/navigations/Comment/CommentTab.tsx
@@ -3,17 +3,18 @@ import MyRequest from '../../components/Comment/MyRequest';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import HomeScreen from '../../screens/Comment/HomeScreen';
 import MyPageScreen from '../../screens/Comment/MyPageScreen';
+import { Colors } from '../../components/renewal';
 
 const Tab = createBottomTabNavigator();
 
 export const CommentTab = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator screenOptions={{ tabBarActiveTintColor: Colors.Red.Default }}>
       <Tab.Screen
         name="홈"
         component={HomeScreen}
         options={{
-          tabBarIcon: ({ color, size }) => <MaterialCommunityIcons name="cards-diamond" color={color} size={size} />,
+          tabBarIcon: ({ color, size }) => <MaterialCommunityIcons name="home-variant" color={color} size={size} />,
           headerShown: false,
         }}
       />
@@ -21,7 +22,7 @@ export const CommentTab = () => {
         name="MY의뢰"
         component={MyRequest}
         options={{
-          tabBarIcon: ({ color, size }) => <MaterialCommunityIcons name="cards-diamond" color={color} size={size} />,
+          tabBarIcon: ({ color, size }) => <MaterialCommunityIcons name="text-box" color={color} size={size} />,
           headerStyle: { shadowOpacity: 0 },
         }}
       />
@@ -29,7 +30,7 @@ export const CommentTab = () => {
         name="마이페이지"
         component={MyPageScreen}
         options={{
-          tabBarIcon: ({ color, size }) => <MaterialCommunityIcons name="cards-diamond" color={color} size={size} />,
+          tabBarIcon: ({ color, size }) => <MaterialCommunityIcons name="account" color={color} size={size} />,
           headerStyle: { shadowOpacity: 0 },
         }}
       />
