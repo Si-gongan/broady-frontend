4f3bb2013dd961bf4b31dbcd3c58e885c3d5168e
rbgksqkr <rbgks1937@naver.com>
2023-10-01 21:07:12 +0900

    Refactor: #82 - data 한곳에 모아두기

--- src/components/Comment/CommentWriting/BottomSheet/ReportBottomSheet.tsx
+++ src/components/Comment/CommentWriting/BottomSheet/ReportBottomSheet.tsx
@@ -6,30 +6,33 @@ import { RadioButton } from 'react-native-paper';
 import { commentFont } from '../../styles';
 import { Colors } from '../../../renewal';
 
+const data = {
+  image: {
+    questionList: [
+      { id: 0, text: '질문과 관계 없는 사진입니다.' },
+      { id: 1, text: '사진을 제대로 식별할 수 없습니다.' },
+      { id: 2, text: '기타' },
+    ],
+    title: '잘못된 사진 제보',
+    text: '의뢰자에게 사진에 어떤 문제가 있는지 구체적으로 알려주세요.\n 제보 내용은 의뢰자에게 즉시 전달됩니다.',
+  },
+  request: {
+    questionList: [
+      { id: 0, text: '부적절한 사진입니다.' },
+      { id: 1, text: '부적절한 질문입니다.' },
+      { id: 2, text: '기타' },
+    ],
+    title: '부적절한 의뢰 신고',
+    text: '사진 및 질문이 (선정성, 폭력성, 사기 등) 부적절하다면\n 운영진에게 알려주세요.',
+  },
+};
+
 interface IReportImageBottomSheetProps {
   postId: string;
   category: number;
   visible: boolean;
   setVisible: React.Dispatch<React.SetStateAction<boolean>>;
 }
-
-const selectImageList = [
-  { id: 0, text: '질문과 관계 없는 사진입니다.' },
-  { id: 1, text: '사진을 제대로 식별할 수 없습니다.' },
-  { id: 2, text: '기타' },
-];
-const selectRequestList = [
-  { id: 0, text: '부적절한 사진입니다.' },
-  { id: 1, text: '부적절한 질문입니다.' },
-  { id: 2, text: '기타' },
-];
-
-const imageTitle = '잘못된 사진 제보';
-const requestTitle = '부적절한 의뢰 신고';
-
-const imageText =
-  '의뢰자에게 사진에 어떤 문제가 있는지 구체적으로 알려주세요.\n 제보 내용은 의뢰자에게 즉시 전달됩니다.';
-const requestText = '사진 및 질문이 (선정성, 폭력성, 사기 등) 부적절하다면\n 운영진에게 알려주세요.';
 
 const ReportBottomSheet = ({ postId, category, visible, setVisible }: IReportImageBottomSheetProps) => {
   const [reportInput, setReportInput] = useState<string>('');
@@ -49,49 +52,24 @@ const ReportBottomSheet = ({ postId, category, visible, setVisible }: IReportIma
     <BottomSheet visible={visible} onBackButtonPress={onClose} onBackdropPress={onClose}>
       <View style={[styles.container, SigonganColor.backgroundPrimary]}>
         <View style={styles.titleWrapper}>
-          <Text style={styles.titleText}>{category ? requestTitle : imageTitle}</Text>
+          <Text style={styles.titleText}>{category ? data.request.title : data.image.title}</Text>
         </View>
         <View style={SigonganDesign.borderOpaque} />
         <View style={styles.itemWrapper}>
           <View style={styles.titleContainer}>
-            <Text style={[commentFont.SMALL_TITLE, styles.description]}>{category ? requestText : imageText}</Text>
+            <Text style={[commentFont.SMALL_TITLE, styles.description]}>
+              {category ? data.request.text : data.image.text}
+            </Text>
           </View>
-
-          <View style={{ width: '90%' }}>
-            {category
-              ? selectRequestList.map((question) => (
-                  <View key={question.id} style={styles.questionContainer}>
-                    <View style={styles.radioBtnContainer}>
-                      <RadioButton
-                        color="black"
-                        value={question.text}
-                        status={selected === question.id ? 'checked' : 'unchecked'}
-                        onPress={() => setSelected(question.id)}
-                      />
-                    </View>
-                    <Text style={[commentFont.BODY1, styles.questionText]}>{question.text}</Text>
-                  </View>
-                ))
-              : selectImageList.map((question) => (
-                  <View key={question.id} style={styles.questionContainer}>
-                    <View style={styles.radioBtnContainer}>
-                      <RadioButton
-                        color="black"
-                        value={question.text}
-                        status={selected === question.id ? 'checked' : 'unchecked'}
-                        onPress={() => setSelected(question.id)}
-                      />
-                    </View>
-                    <Text style={[commentFont.BODY1, styles.questionText]}>{question.text}</Text>
-                  </View>
-                ))}
+          <View style={styles.questionListContainer}>
+            <QuestionList category={category} selected={selected} setSelected={setSelected} />
           </View>
         </View>
-        <View style={{ marginBottom: 15 }}>
-          <View style={{ flexDirection: 'row', justifyContent: 'flex-end', marginHorizontal: 20 }}>
+        <View style={styles.inputContainer}>
+          <View style={styles.inputLengthContainer}>
             <Text style={commentFont.BODY2}>{reportInput.length} / 100</Text>
           </View>
-          <View style={{ alignItems: 'center' }}>
+          <View style={styles.textContainer}>
             <TextInput
               placeholder="제보할 내용을 작성해주세요"
               multiline
@@ -113,6 +91,40 @@ const ReportBottomSheet = ({ postId, category, visible, setVisible }: IReportIma
         </View>
       </View>
     </BottomSheet>
+  );
+};
+
+const QuestionList = ({ category, selected, setSelected }: any) => {
+  return (
+    <>
+      {category
+        ? data.request.questionList.map((question) => (
+            <View key={question.id} style={styles.questionContainer}>
+              <View style={styles.radioBtnContainer}>
+                <RadioButton
+                  color="black"
+                  value={question.text}
+                  status={selected === question.id ? 'checked' : 'unchecked'}
+                  onPress={() => setSelected(question.id)}
+                />
+              </View>
+              <Text style={[commentFont.BODY1, styles.questionText]}>{question.text}</Text>
+            </View>
+          ))
+        : data.image.questionList.map((question) => (
+            <View key={question.id} style={styles.questionContainer}>
+              <View style={styles.radioBtnContainer}>
+                <RadioButton
+                  color="black"
+                  value={question.text}
+                  status={selected === question.id ? 'checked' : 'unchecked'}
+                  onPress={() => setSelected(question.id)}
+                />
+              </View>
+              <Text style={[commentFont.BODY1, styles.questionText]}>{question.text}</Text>
+            </View>
+          ))}
+    </>
   );
 };
 
@@ -141,6 +153,20 @@ const styles = StyleSheet.create({
   description: {
     color: '#767676',
     textAlign: 'center',
+  },
+  questionListContainer: {
+    width: '90%',
+  },
+  inputLengthContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    marginHorizontal: 20,
+  },
+  inputContainer: {
+    marginBottom: 15,
+  },
+  textContainer: {
+    alignItems: 'center',
   },
   questionContainer: {
     flexDirection: 'row',
