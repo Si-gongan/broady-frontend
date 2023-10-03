fab3464722cdb15745cc4b3f6b6b4a51f971653e
rbgksqkr <rbgks1937@naver.com>
2023-10-03 23:58:07 +0900

    Feat: #86 - 푸시알림 연동

---
+++ src/components/Comment/Mypage/AlertExtraQuestion.tsx
@@ -0,0 +1,104 @@
+import { useEffect, useState } from 'react';
+import { Alert, View, Text, Switch, StyleSheet, Linking } from 'react-native';
+import { useRecoilValue } from 'recoil';
+import { ChangeAlarmStatus, GetAlarmStatus } from '../../../api/axios';
+import { fcmTokenState } from '../../../states';
+import { getNotificationPermissions } from '../../common/notifications';
+import { commentFont } from '../styles';
+import * as Notifications from 'expo-notifications';
+
+const AlertExtraQuestion = () => {
+  const fcmToken = useRecoilValue(fcmTokenState);
+  const [isEnabled, setIsEnabled] = useState(false);
+
+  const toggleSwitch = async () => {
+    const newState = !isEnabled;
+
+    if (newState === false) {
+      try {
+        setIsEnabled(false);
+        await ChangeAlarmStatus(false, fcmToken);
+      } catch {
+        setIsEnabled(true);
+      }
+
+      return;
+    }
+
+    try {
+      setIsEnabled(true);
+
+      const state = await getNotificationPermissions();
+
+      if (state !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        if (status !== 'granted') {
+          Alert.alert('알림', '설정에서 알림을 켜주세요.', [
+            {
+              text: '확인',
+              style: 'default',
+              onPress: () => Linking.openSettings(),
+            },
+          ]);
+        }
+      }
+
+      await ChangeAlarmStatus(true, fcmToken);
+    } catch {
+      setIsEnabled(false);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const state = await GetAlarmStatus(fcmToken);
+        setIsEnabled(state.data.result.isAccepted);
+      } catch {
+        setIsEnabled(false);
+      }
+    })();
+  }, []);
+
+  return (
+    <View style={styles.alertContainer}>
+      <View style={styles.toggleContainer}>
+        <Text style={commentFont.TITLE}>추가 질문 알림</Text>
+        <Switch
+          trackColor={{ false: '#767676', true: '#AEB8F4' }}
+          thumbColor={isEnabled ? '#fff' : '#fff'}
+          ios_backgroundColor="#767676"
+          onValueChange={toggleSwitch}
+          value={isEnabled}
+        />
+      </View>
+      <Text style={[commentFont.SMALL_TITLE, styles.blackText]}>
+        내가 작성한 해설에 대해, 시각장애인의 추가 질문이{'\n'}들어올 경우 푸시 알림을 수신합니다.
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  alertContainer: {
+    width: '80%',
+    justifyContent: 'center',
+    marginVertical: 20,
+  },
+  toggleContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingBottom: 15,
+  },
+  blackText: {
+    color: 'black',
+  },
+  divisionLine: {
+    width: '100%',
+    height: 1,
+    backgroundColor: '#E8E8E8',
+  },
+});
+
+export default AlertExtraQuestion;
--- src/screens/Comment/AlertSettingScreen.tsx
+++ src/screens/Comment/AlertSettingScreen.tsx
@@ -1,59 +1,16 @@
-import { useState } from 'react';
-import { View, Text, StyleSheet, Switch } from 'react-native';
-import { useRecoilValue } from 'recoil';
-import { commentFont } from '../../components/Comment/styles';
+import { View, StyleSheet } from 'react-native';
+import AlertExtraQuestion from '../../components/Comment/Mypage/AlertExtraQuestion';
+import AlertNewRequest from '../../components/Comment/Mypage/AlertNewRequest';
 import Header from '../../components/common/Header';
-import { authTokenState, fcmTokenState } from '../../states';
 
 const AlertSettingScreen = () => {
-  const fcmToken = useRecoilValue(fcmTokenState);
-  const authToken = useRecoilValue(authTokenState);
-  const [isExtraQuestion, setIsExtraQuestion] = useState<boolean>(false);
-  const [isNewRequest, setIsNewRequest] = useState<boolean>(false);
-
-  const toggleExtraSwitch = () => {
-    setIsExtraQuestion((prev) => !prev);
-  };
-
-  const toggleRequestSwitch = () => {
-    setIsNewRequest((prev) => !prev);
-  };
-
   return (
     <View>
       <Header isBack={true}>알림 설정</Header>
       <View style={styles.bodyContainer}>
-        <View style={styles.alertContainer}>
-          <View style={styles.toggleContainer}>
-            <Text style={commentFont.TITLE}>추가 질문 알림</Text>
-            <Switch
-              trackColor={{ false: '#767676', true: '#AEB8F4' }}
-              thumbColor={isExtraQuestion ? '#fff' : '#fff'}
-              ios_backgroundColor="#767676"
-              onValueChange={toggleExtraSwitch}
-              value={isExtraQuestion}
-            />
-          </View>
-          <Text style={[commentFont.SMALL_TITLE, styles.blackText]}>
-            내가 작성한 해설에 대해, 시각장애인의 추가 질문이{'\n'}들어올 경우 푸시 알림을 수신합니다.
-          </Text>
-        </View>
+        <AlertExtraQuestion />
         <View style={styles.divisionLine} />
-        <View style={styles.alertContainer}>
-          <View style={styles.toggleContainer}>
-            <Text style={commentFont.TITLE}>해설의뢰 알림</Text>
-            <Switch
-              trackColor={{ false: '#767676', true: '#AEB8F4' }}
-              thumbColor={isNewRequest ? '#fff' : '#fff'}
-              ios_backgroundColor="#767676"
-              onValueChange={toggleRequestSwitch}
-              value={isNewRequest}
-            />
-          </View>
-          <Text style={[commentFont.SMALL_TITLE, styles.blackText]}>
-            시각장애인의 새로운 사진 해설 의뢰가 업로드될 경우,{'\n'}일정한 간격으로 푸시 알림을 수신합니다.
-          </Text>
-        </View>
+        <AlertNewRequest />
       </View>
     </View>
   );
@@ -62,20 +19,6 @@ const AlertSettingScreen = () => {
 const styles = StyleSheet.create({
   bodyContainer: {
     alignItems: 'center',
-  },
-  alertContainer: {
-    width: '80%',
-    justifyContent: 'center',
-    marginVertical: 20,
-  },
-  toggleContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingBottom: 15,
-  },
-  blackText: {
-    color: 'black',
   },
   divisionLine: {
     width: '100%',
