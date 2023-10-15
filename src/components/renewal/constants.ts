4595ce48f4af79bdbf4b1392923ba18da0fdfdda
Min-jae Kim <mink4661@snu.ac.kr>
2023-10-16 03:14:39 +0900

    feat: modify one_to_one link

--- src/components/renewal/constants.ts
+++ src/components/renewal/constants.ts
@@ -1,3 +1,3 @@
-export const ONE_TO_ONE_QUESTION = 'http://pf.kakao.com/_sAnVG';
+export const ONE_TO_ONE_QUESTION = 'http://pf.kakao.com/_pKUjG';
 export const PRIVACY_POLICY = 'https://sites.google.com/view/bomjaguk/개인정보처리방침?authuser=0';
 export const TERMS_OF_USE = 'https://sites.google.com/view/bomjaguk/이용약관';
